98859f6fafc24ff186d729ac0febe3b571a82ed3
Daniel Levi-Minzi <daniel.levi-minzi@shipt.com>
2022-07-07 18:10:02 -0400

    add remote file and share remote file ;)

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
-    port: process.env.PORT || 3000
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000,
 });
 
-app.message('hello', async({message, say}) => {
-    await say(`Hey there <@${message.user}>!`);
-    console.log('hi');
+app.message("hello", async ({ message, say }) => {
+  await say(`Hey there <@${message.user}>!`);
+  console.log("hi");
 });
 
-app.shortcut('example', async({shortcut, body, ack, client, logger}) => {
-    try {
-        await ack();
-
-        const result = await client.views.open({
-            trigger_id: body.trigger_id,
-            view: {
-                type: 'modal',
-                // View identifier
-                callback_id: 'view_1',
-                title: {
-                  type: 'plain_text',
-                  text: 'Modal title'
-                },
-            blocks: [
-              {
-                type: 'input',
-                block_id: 'input_c',
-                label: {
-                  type: 'plain_text',
-                  text: 'What are your hopes and dreams?'
-                },
-                element: {
-                  type: 'plain_text_input',
-                  action_id: 'dreamy_input',
-                  multiline: true
-                }
-              }
-            ],
-            submit: {
-              type: 'plain_text',
-              text: 'Submit'
-            }
-          }
-        });
-
-    logger.info(result);
-    }
-    catch(error) {
-        logger.error(error);
-    }
-});
-
-app.view('view_1', async({ack, body, view, client, logger }) => {
+app.shortcut("example", async ({ shortcut, body, ack, client, logger }) => {
+  try {
     await ack();
 
-    const val = view['state']['values']['input_c']['dreamy_input']['value'];
-    const user = body['user']['id'];
+    const result = await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: "modal",
+        // View identifier
+        callback_id: "view_1",
+        title: {
+          type: "plain_text",
+          text: "Modal title",
+        },
+        blocks: [
+          {
+            type: "input",
+            block_id: "input_c",
+            label: {
+              type: "plain_text",
+              text: "What are your hopes and dreams?",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "dreamy_input",
+              multiline: true,
+            },
+          },
+        ],
+        submit: {
+          type: "plain_text",
+          text: "Submit",
+        },
+      },
+    });
 
-    console.log(val);
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+});
 
-    try {
+app.view("view_1", async ({ ack, body, view, client, logger }) => {
+  await ack();
+
+  const val = view["state"]["values"]["input_c"]["dreamy_input"]["value"];
+  const user = body["user"]["id"];
+
+  console.log(val);
+
+  await client.files.remote.add({title: 'test', external_url: 'https://cdn2.thecatapi.com/images/p_6R6WwtN.jpg', external_id: 'id01299', filetype: 'jpg'})
+  client.files.remote.share({channels: "C03AHRFL153,D03NPUWJQBU", external_id: 'id01299'})
+
+  try {
     await client.chat.postMessage({
       channel: user,
-      text: val
-        });
-      }
-      catch (error) {
-        logger.error(error);
-      }
-
+      text: val,
+    });
+  } catch (error) {
+    logger.error(error);
+  }
 });
 
 (async () => {
-    await app.start();
+  await app.start();
 
-    console.log('⚡️ app running!');
+  console.log("⚡️ app running!");
 })();
-
